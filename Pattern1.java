07131f6719ae8e1cafaf4ade2fbae8c84b86ec56
vaibhavgaikwad8476 <vaibhavgaikwad8475@gmail.com>
2024-09-10 00:49:49 +0530

    add code

---
+++ Pattern1.java
@@ -0,0 +1,13 @@
+class Pattern1
+{
+	public static void main(String[] args) {
+		for(int i = 1; i <=6; i++){
+			for(int j = 1; j<=6; j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		
+	}
+}
